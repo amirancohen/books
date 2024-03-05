aa15b326d6bd064dd435724013028d74e1eec138
amiran cohen <amirancohen@hotmail.com>
2024-03-05 20:29:22 +0200

    add commit  final

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,9 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
 import "./index.css";
-// import reportWebVitals from "./reportWebVitals";
+import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap-icons/font/bootstrap-icons.css";
+var ReactDOM = require('react-dom/client');
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
