5245739caf65696f16a9f414926454258b6c5c22
amiran cohen <amirancohen@hotmail.com>
2024-03-06 21:58:08 +0200

    add mongo fix

--- server/config/dev.js
+++ server/config/dev.js
@@ -1,5 +1,5 @@
 module.exports = {
-    // db_conn: process.env.MONGO_URI,
-    db_conn: 'mongodb://localhost:27017/einat_editor',
+    db_conn: process.env.MONGO_URI,
+    // db_conn: 'mongodb://localhost:27017/einat_editor',
     jwt_token: 'myPrivateKey',
 }
