ca8ebc3f42c7e361424cae6140a2ea31ccdeb494
amiran cohen <amirancohen@hotmail.com>
2024-03-07 19:58:43 +0200

    add delete user

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,23 +1,22 @@
 import { useEffect } from "react";
-
+import { useUserData } from "../context/user";
+import { getUserData } from "../services/ApiService";
 import About from "../components/About";
 import MyServices from "../components/MyServices";
 import Recommendations from "../components/Recommendations";
 import MyWorks from "../components/MyWorks";
 import AddRecommendation from "../components/addRecommendation";
-import { useUserData } from "../context/user";
-import { getUserData } from "../services/ApiService";
 
 
 
 
 function Homepage() {
-const user = useUserData();
-useEffect(() => {
-  getUserData().then((res) => {
-    user?.setUser(res);
-  });
-}, []);
+// const user = useUserData();
+// useEffect(() => {
+//   getUserData().then((res) => {
+//     user?.setUser(res);
+//   });
+// }, []);
   return (
     <>
       <About />
