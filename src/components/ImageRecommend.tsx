90a42db33cd19d8e60eb21958accbcbdc3538e09
amiran cohen <amirancohen@hotmail.com>
2024-05-31 21:32:06 +0300

    fiz size pic

--- src/components/ImageRecommend.tsx
+++ src/components/ImageRecommend.tsx
@@ -6,6 +6,7 @@ import Box from "@mui/material/Box";
 import image1 from "./pic/image1.jpg";
 import image2 from "./pic/image2.jpg";
 import { Image } from "mui-image";
+import Card from "@mui/material/Card";
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
@@ -20,12 +21,16 @@ function ImageRecommend() {
       <Box >
         <Grid  xs={12} sm={8} >
           <Item >
+          <Card sx={{ maxWidth: 600, margin: "auto" }}>
             <Image alt="Einat Zilberman" src={image1} style={{width:'400px', textAlign:'center'}} />
+          </Card>
           </Item>
         </Grid>
         <Grid item xs={12} sm={8}>
           <Item >
+          <Card sx={{ maxWidth: 600, margin: "auto" }}>
             <Image alt="Einat Zilberman" src={image2} style={{width:'400px'}} />
+          </Card>
           </Item>
         </Grid>
       </Box>
